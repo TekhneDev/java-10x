9399b7e4f7c3cc1ffc5756467c11e090170a565f
Carla Mariana <carlamab2808@outlook.com>
2025-06-05 23:38:25 -0300

    add

---
+++ challenges-java-10x/challenge-one/DesafioUm/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        // Ninja 1
+        String nomeNinja1 = "Naruto Uzumaki";
+        int idade = 18;
+        String missao = "Encontrar o vilão";
+        String statusDaMissao = "Em andamento";
+        char nivelDaMissao = 'A';
+
+        System.out.println("Nome do ninja: " + nomeNinja1);
+
+        // Ninja 2
+        String nomeNinja2 = "Sasuke Uchiha";
+        int idade2 = 18;
+        String missao2 = "Reconhecimento da aldeia";
+        String statusDaMissao2 = "Em andamento";
+        char nivelDaMissao2 = 'S';
+
+        // Ninja 3
+        String nomeNinja3 = "Sakura Haruno";
+        int idade3 = 18;
+        String missao3 = "Curar feridos da guerra";
+        String statusDaMissao3 = "Concluido";
+        char nivelDaMissao3 = 'D';
+
+
+
+    }
+}
